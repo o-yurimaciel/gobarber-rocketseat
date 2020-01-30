57cd76eebd84bdd8290040e36ec136b8c5fd7f5e
Yuri Maciel <54074819+yrimaciel@users.noreply.github.com>
2020-01-29 23:16:01 -0300

    fix providers endpoint

--- src/routes.js
+++ src/routes.js
@@ -19,7 +19,7 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.get('/proviers', ProviderController.index);
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
