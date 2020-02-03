b2153051122245b0a548958419ab8569fb43548b
Yuri Maciel <54074819+yrimaciel@users.noreply.github.com>
2020-02-02 23:51:01 -0300

    nodemailer to appointment cancel with hbs template

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -4,6 +4,7 @@ import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Mail from '../../lib/Mail';
 
 import Notification from '../schemas/Notification';
 
@@ -104,7 +105,20 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
+    });
 
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
@@ -123,6 +137,19 @@ class AppointmentController {
     appointment.canceled_at = new Date();
 
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}`,
+      subject: 'Agendamento cancelado',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia dd 'de' MMMM', às' H:mm'h", {
+          locale: pt,
+        }),
+      },
+    });
 
     return res.json(appointment);
   }
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'c93685e968485f',
+    pass: '0b68562b965d54',
+  },
+  default: {
+    from: 'Yuri Maciel <noreply@gobarber.com>',
+  },
+};
