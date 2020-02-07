7c465577af34d2f6acc18b034cc77910868d350d
Yuri Maciel <54074819+yrimaciel@users.noreply.github.com>
2020-02-07 10:39:38 -0300

    .env

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '6a866dac5fbfc352051d3e36e1af39f6',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'c93685e968485f',
-    pass: '0b68562b965d54',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Yuri Maciel <noreply@gobarber.com>',
